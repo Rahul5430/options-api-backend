3caae5481dcda3bd6969a51cbe9ea39dfbece4b4
Rahul5430 <rahul213himani@gmail.com>
2021-05-10 14:45:44 +0530

    testing

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask_restful import Resource, Api
 from nsetools import Nse
 from nsepython import *
 from flask_cors import CORS, cross_origin
+from bs4 import BeautifulSoup
+import requests
 # from pprint import pprint
 
 app = Flask(__name__)
@@ -15,6 +17,16 @@ nse = Nse()
 @app.route('/')
 def quote():
     return "Hello!"
+
+@app.route('/1')
+def temp():
+    url = 'https://www.moneycontrol.com/indices/fno/view-option-chain/NIFTY/2021-05-12'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    tables = soup.find_all('table')
+    # print(1)
+    # print(tables[1])
+    return str(tables[1])
 
 class NseData(Resource):
     def get(self, quote):
